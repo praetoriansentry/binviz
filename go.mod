7a4e0d6df30424e15781c405aee038fc40f5bcec
John Hilliard <jhilliard@nextjump.com>
2021-07-11 18:42:29 -0400

    adding some hue varation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module binviz
+
+go 1.16
+
+require github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"math"
 	"os"
+
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 const (
@@ -19,8 +21,9 @@ const (
 
 type (
 	Coord struct {
-		X int
-		Y int
+		X     int
+		Y     int
+		Index int
 	}
 )
 
@@ -33,6 +36,8 @@ func main() {
 
 	coords := make([]Coord, 0)
 	counts := make(map[string]int)
+	indexes := make(map[string]int)
+
 	for i := 0; i+3 < len(bin); i = i + 1 {
 		a := int(bin[i])
 		b := int(bin[i+1])
@@ -46,15 +51,18 @@ func main() {
 		var yF float64 = (float64(y) / float64(MAX_DIM)) * float64(IMAGE_WIDTH)
 
 		coord := Coord{
-			X: int(math.Round(xF)),
-			Y: int(math.Round(yF)),
+			X:     int(math.Round(xF)),
+			Y:     int(math.Round(yF)),
+			Index: i,
 		}
 		key := fmt.Sprintf("%d-%d", coord.X, coord.Y)
-		log.Println(key)
 		keyCount, hasKey := counts[key]
+		indexTotal := indexes[key]
 		if !hasKey {
 			keyCount = 0
+			indexTotal = 0
 		}
+		indexes[key] = indexTotal + i
 		counts[key] = keyCount + 1
 		coords = append(coords, coord)
 	}
@@ -69,9 +77,23 @@ func main() {
 	r := image.Rect(0, 0, int(IMAGE_WIDTH), int(IMAGE_HEIGHT))
 	img := image.NewRGBA(r)
 
+	for x := 0; x < IMAGE_WIDTH; x += 1 {
+		for y := 0; y < IMAGE_HEIGHT; y += 1 {
+			img.Set(x, y, color.RGBA{0, 0, 0, 255})
+		}
+	}
+
 	for _, coord := range coords {
-		//key := fmt.Sprintf("%d-%d", coord.X, coord.Y)
-		img.Set(coord.X, coord.Y, color.RGBA{255, 255, 255, 255})
+		key := fmt.Sprintf("%d-%d", coord.X, coord.Y)
+		keyCount := float64(counts[key])
+		indexTotal := float64(indexes[key])
+
+		var freq float64 = keyCount / float64(maxCount)
+		var coeff = float64(maxCount / 2)
+		alpha := 1.0 - math.Exp(-1.0*math.Pow(coeff*freq, 2))
+		avgPosition := (indexTotal / keyCount) / float64(len(bin))
+		c := colorful.Hsv(avgPosition*360.0, 1, alpha)
+		img.Set(coord.X, coord.Y, c)
 	}
 
 	f, err := os.Create("img.png")
